ea562566b00d25a46b416dc290429d94f58540a8
denisaradu18 <149544551+denisaradu18@users.noreply.github.com>
2024-11-26 00:04:55 +0200

    Seminarul 8

--- Seminar4/app/src/main/java/com/example/seminar4/MainActivity.java
+++ Seminar4/app/src/main/java/com/example/seminar4/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 Masina masina=data.getParcelableExtra("masina");
                 masini.add(masina);
             }
+
         }
     }
 
